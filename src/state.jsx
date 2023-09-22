651f906dbf6c87f51f9b110c23ff4c808a354671
Nehutu <valitov.renat@gmail.com>
2023-09-22 09:47:35 +0300

    Server connection

--- src/state.jsx
+++ src/state.jsx
@@ -8,48 +8,50 @@ const user = {
   about: "Something about me",
 };
 
-const users = {
-  0: {
-    id: 5,
-    name: "Eve",
-    surname: "Sunders",
-    email: "eve@ya.ru",
-    avatar:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80",
-  },
-  1: {
-    id: 3,
-    name: "Joe",
-    surname: "Joers",
-    email: "joe@google.cn",
-    avatar:
-      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",
-  },
-  2: {
-    id: 13,
-    name: "Kate",
-    surname: "Sunders",
-    email: "kate@ya.ru",
-    avatar:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&w=300&q=80",
-  },
-  3: {
-    id: 6,
-    name: "Lou",
-    surname: "Fishes",
-    email: "storm@hop.cz",
-    avatar:
-      "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",
-  },
-  4: {
-    id: 7,
-    name: "Luka",
-    surname: "Brazzi",
-    email: "sleepy@PureComponent.su",
-    avatar:
-      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",
-  },
-};
+// const users = {
+//   0: {
+//     id: 5,
+//     name: "Eve",
+//     surname: "Sunders",
+//     email: "eve@ya.ru",
+//     avatar:
+//       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80",
+//   },
+//   1: {
+//     id: 3,
+//     name: "Joe",
+//     surname: "Joers",
+//     email: "joe@google.cn",
+//     avatar:
+//       "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",
+//   },
+//   2: {
+//     id: 13,
+//     name: "Kate",
+//     surname: "Sunders",
+//     email: "kate@ya.ru",
+//     avatar:
+//       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&w=300&q=80",
+//   },
+//   3: {
+//     id: 6,
+//     name: "Lou",
+//     surname: "Fishes",
+//     email: "storm@hop.cz",
+//     avatar:
+//       "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",
+//   },
+//   4: {
+//     id: 7,
+//     name: "Luka",
+//     surname: "Brazzi",
+//     email: "sleepy@PureComponent.su",
+//     avatar:
+//       "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",
+//   },
+// };
+
+let users = [];
 
 export function getUser(userId) {
   for (let i = 0; i < Object.keys(users).length; i++) {
@@ -60,6 +62,9 @@ export function getUser(userId) {
   return user;
 }
 
-export function getUsers() {
+export async function getUsers() {
+  let response = await fetch("http://yasmin.p-host.in/getUsers");
+  users = await response.json();
+  console.log(users);
   return users;
 }
